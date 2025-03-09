1afcce14bd9c0689149460000f5c53438db76b1b
apple <apple@apples-MacBook-Air.local>
2025-03-09 16:04:55 +0530

    add jwt authGuard to protect routes

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -56,4 +56,18 @@ export class AuthService {
       accessToken,
     };
   }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return { id: user.id };
+  }
 }
--- apps/api/src/post/post.resolver.ts
+++ apps/api/src/post/post.resolver.ts
@@ -3,11 +3,14 @@ import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
   findAll() {
     return this.postService.findAll();
