c6439d959752f2c8a0bc9abdbe801c7d2d8c1aaa
apple <apple@apples-MacBook-Air.local>
2025-03-09 13:36:32 +0530

    validate local user in signin mutation

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -1,7 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
+import { SignInInput } from './dto/signin-input';
+import { User } from 'src/user/entities/user.entity';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => User)
+  async signIn(@Args('signInInput') signInInput: SignInInput) {
+    const user = await this.authService.validateLocalUser(signInInput);
+    return user;
+  }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SignInInput } from './dto/signin-input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { verify } from 'argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async validateLocalUser({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found with this email');
+    }
+
+    if (!user.password) {
+      throw new UnauthorizedException('Please Enter Password');
+    }
+    const isPasswordValid = await verify(user.password, password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return user;
+  }
+}
